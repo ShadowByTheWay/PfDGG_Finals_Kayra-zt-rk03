070328b8c025decf0cbd8e636ffcb3701ea365d3
ShadowByTheWay <ozturkkayr@mef.edu.tr>
2026-01-07 20:52:16 +0300

    I really hate github

---
+++ Order.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheFinals
+{
+    internal class Order
+    {
+        public string from;
+        public string to;
+        public int pay;
+        public int deadline;
+        public bool completed;
+
+        
+
+    }
+}
